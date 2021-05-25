fa0ab77c9388a4f7811fc6fa8a71f6efbe1c8fce
railsgirl <paulina.recko@gmail.com>
2021-05-25 22:08:36 +0200

    Add Reservation and Ticket models with relations

---
+++ db/migrate/20210525200421_create_tickets.rb
@@ -0,0 +1,11 @@
+class CreateTickets < ActiveRecord::Migration[6.1]
+  def change
+    create_table :tickets do |t|
+      t.integer :price
+      t.string :tariff
+      t.references :reservation, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
