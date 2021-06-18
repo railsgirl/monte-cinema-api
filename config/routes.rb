b21b8c97833ea45ede547ea0a77c41fe76ef8f06
railsgirl <39632441+railsgirl@users.noreply.github.com>
2021-06-18 13:45:14 +0200

    Online offline reservations (#6)
    * Enable offline reservations creation
    
    * Enable online reservations

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
-  resources :ticket_desks
-  resources :reservations
   resources :movies
   resources :seances
   resources :cinema_halls
+
+  resources :ticket_desks do
+    resources :reservations do
+      collection do
+        post '/offline', to: 'reservations#create_offline'
+        post '/online', to: 'reservations#create_online'
+      end
+    end
+  end
 end
