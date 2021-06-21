79b92e728a7ee17fc5ba9f5af2256a16a35899f9
railsgirl <paulina.recko@gmail.com>
2021-06-21 21:21:47 +0200

    Add controller and routes for tickets

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,13 @@ Rails.application.routes.draw do
 
   resources :ticket_desks do
     resources :reservations do
+
       collection do
         post '/offline', to: 'reservations#create_offline'
         post '/online', to: 'reservations#create_online'
       end
+
+      resources :tickets
     end
   end
 end
