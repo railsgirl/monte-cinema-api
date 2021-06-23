51b75dbd8093fd225d766181cb5fadef7c71b8d9
railsgirl <paulina.recko@gmail.com>
2021-06-23 08:29:36 +0200

    Add reservation status validation

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,6 +1,16 @@
 class Reservation < ApplicationRecord
+  STATUSES = ["booked", "paid", "cancelled"]
+
   belongs_to :seance
   belongs_to :client
   belongs_to :ticket_desk
   has_many :tickets, dependent: :destroy
+
+  validate :has_valid_status
+
+  def has_valid_status
+    if STATUSES.exclude?(status)
+      errors.add(:status, "invalid status")
+    end
+  end
 end
